ae74c1a0e579eea159b09751d844bb508d07a60a
Aminul-hoq <61986445+Aminul-hoq@users.noreply.github.com>
2021-04-22 20:33:49 +0600

    fixme

--- pdfToJson.py
+++ pdfToJson.py
@@ -1,24 +1,27 @@
 from pdfminer.pdfpage import PDFPage
 import textToDictionary
-
+import argparse
 import json
+import os
+from PyPDF2 import PdfFileReader
 
-list =[]
+list = []
+ap = argparse.ArgumentParser()
+ap.add_argument("-f", "--file", required=True,
+                help="path to input directory of pdf")
+args = vars(ap.parse_args())
 
 
-def pdfParser():
-
-    fp = open("2020-cdp-water-response.pdf", 'rb')
-
+def pdfParser(args):
+    fp = open(str((args["file"])))
+    pdf = PdfFileReader(fp)
     # Create a PDF interpreter object.
     # Process each page contained in the document.
-    counter = 1
-    create_file = open("abc.json", "a")
+    create_file = open("export.json", "a")
     for page in PDFPage.get_pages(fp):
         page_data = textToDictionary.converter(page, counter)
-        if (len(page_data) != 0):
+        if len(page_data) != 0:
             list.append(page_data)
-        counter += 1
 
     json_data = json.dumps(list, indent=2)
     create_file.write(json_data)
@@ -26,4 +29,4 @@ def pdfParser():
 
 
 if __name__ == '__main__':
-    pdfParser()
+    pdfParser(args)
--- textToDictionary.py
+++ textToDictionary.py
@@ -25,41 +25,18 @@ def groupData(data):
     # pattern1 = "W\d\."
     pattern2 = "W\d\.\d|W\d\.\d\w|W...\d\.\d\w"
     group_key = (re.findall(pattern2, data))
-    # if not group_key:
-    #     group["value"] = data
-    # for key in group_key:
-    #     group_with_key = {}
-    #     for s in data.split("\n\n"):
-    #         # print(s + "\n\n\n\n")
-    #         if key in s:
-    #             sub_group_key = re.findall(pattern2, s)
-    #             for sub_key in sub_group_key:
-    #                 group_with_sub_key = {}
-    #                 if (sub_key+")" in s):
-    #                     group_with_sub_key["text"] = s
-    #                 else:
-    #                     group_with_sub_key["value"] = s
-    #                 group_with_key[sub_key] = group_with_sub_key
-    #             group[key] = group_with_key
-    #         else:
-    #             sub_group_key = re.findall(pattern2, s)
-    #             for sub_key in sub_group_key:
-    #                 group_with_sub_key = {}
-    #                 if (sub_key + ")" in s):
-    #                     group_with_sub_key["text"] = s
-    #                 else:
-    #                     group_with_sub_key["value"] = s
-    #                 group_with_key[sub_key] = group_with_sub_key
-    #             group[key] = group_with_key
     split = data.split("\n\n")
     for key in group_key:
         group_by_key = {}
+        value = ""
+        txt = ""
         for s in split:
             if "(" + key + ")" in s:
-                group_by_key["text"] = s
-
-            # Fixme - need to remove spacial character from string
+                txt = s
+                continue
             else:
-                group_by_key["value"] = s
+                value += s
+            group_by_key["text"] = txt
+            group_by_key["value"] = value
         group[key] = group_by_key
     return group
